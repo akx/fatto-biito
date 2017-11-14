cb29c130425d37ca9dbafa3d2ade105ee626cc02
Aarni Koskela <akx@iki.fi>
2017-11-14 14:20:48 +0200

    MAKE IT A DEVICE

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@
 const m = require('mithril');
 const JSZip = require('jszip');
 const naturalSort = require('javascript-natural-sort');
+const fattoPixelSrc = require('./fatto-pixel.png');
+const stickerSrc = require('./fatto-sticker.svg');
 require('./style.css');
 
 const audioContext = new AudioContext();
+const outputNode = audioContext.createAnalyser();
+outputNode.fftSize = 128;
+outputNode.connect(audioContext.destination);
 let samples = [];
-const playAnimKeyframes = [
-  { transform: 'scale(0.9)' },
-  { transform: 'scale(1)' },
-];
-
-const playAnimTiming = {
-  duration: 350,
-  iterations: 1,
-};
+let canvas = null;
+const initTime = +new Date();
 
 const keys = 'qwertyuiopasdfghjklzxcvbnm1234567890';
+const fattoPixelImage = Object.assign(new Image(), { src: fattoPixelSrc });
 
 
 function basename(p) {
@@ -41,9 +40,7 @@ function decodeSamples(zipFiles) {
     zipFiles.map((wf) =>
       wf.async('arraybuffer')
         .then((rawData) => audioContext.decodeAudioData(rawData))
-        .then((audioData) => {
-          return { name: basename(wf.name), audioData };
-        })
+        .then((audioData) => ({ name: basename(wf.name), audioData }))
         .catch((error) => ({ name: basename(wf.name), error }))
     )
   );
@@ -65,20 +62,53 @@ function loadZip(file) {
 }
 
 function playSample(sample) {
-  const audioNode = audioContext.createBufferSource();
-  audioNode.buffer = sample.audioData;
-  audioNode.connect(audioContext.destination);
-  audioNode.start();
-  if (sample.buttonElement) {
-    sample.buttonElement.animate(playAnimKeyframes, playAnimTiming);
+  if (sample.audioData) {
+    const audioNode = audioContext.createBufferSource();
+    audioNode.buffer = sample.audioData;
+    audioNode.connect(outputNode);
+    audioNode.start();
+  }
+  if (sample.buttonContainerElement) {
+    sample.buttonContainerElement.classList.add('pressed');
+    setTimeout(() => sample.buttonContainerElement.classList.remove('pressed'), 500);
   }
 }
 
-const view = () => m('main', [
-  m('input', { type: 'file', onchange: (e) => loadZip(e.target.files[0]) }),
+function updateCanvas() {
+  if (!canvas) return;
+  const ctx = canvas.getContext('2d');
+  const time = (+new Date() - initTime);
+  ctx.clearRect(0, 0, 128, 32);
+  ctx.globalAlpha = 0.7;
+  if (!samples.length) {
+    const prob = Math.min(time / 3000, 1);
+    if(Math.random() <= prob) {
+      const x = 38 + Math.sin(+new Date() / 1000) * prob * 20;
+      ctx.drawImage(fattoPixelImage, Math.round(x), 2);
+    }
+  } else {
+    const dataArray = new Uint8Array(outputNode.fftSize);
+    outputNode.getByteTimeDomainData(dataArray);
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    for (let i = 0; i < dataArray.length; i++) {
+      const y = Math.round(dataArray[i] / 255.0 * 32);
+      ctx.rect(i, y, 1, 1);
+    }
+    ctx.fill();
+  }
+}
+
+const sampleButton = (sample, index) => (
   m(
-    'div',
-    samples.map((sample, index) => (
+    '.button-ctr',
+    {
+      key: sample.name,
+      oncreate: (vnode) => {
+        sample.buttonContainerElement = vnode.dom;
+      },
+    },
+    [
       m(
         'button',
         {
@@ -89,15 +119,46 @@ const view = () => m('main', [
           },
         },
         [
-          m('span', sample.name),
+          m('span', sample.name.replace('.wav', '')),
           m('kbd', (keys[index] || '').toUpperCase()),
         ],
-      ))
-    )
+      ),
+      m('.button-3d'),
+    ]
+  )
+);
+
+const view = () => m('main', [
+  m('div#disp', [
+    m('img#sticker', {src: stickerSrc}),
+    m('canvas', {
+      width: 128,
+      height: 32,
+      oncreate: (vnode) => {
+        canvas = vnode.dom;
+        setInterval(updateCanvas, 1000 / 30);
+      },
+    }),
+  ]),
+  m(
+    'div#pad',
+    samples.map((sample, index) => sampleButton(sample, index)),
   ),
+  m('div#drive', [
+    m('#drive-wrap', [
+      (samples.length ? null : m('span', ['Insert ZIP file'])),
+      m('input', { type: 'file', onchange: (e) => loadZip(e.target.files[0]) }),
+    ]),
+  ]),
 ]);
 
+function initFakeSamples(n = 16) {
+  for (let i = 0; i < n; i++) {
+    samples.push({ name: i });
+  }
+}
 
+// initFakeSamples();
 m.mount(document.body, { view });
 
 window.addEventListener('keydown', (e) => {
@@ -107,5 +168,6 @@ window.addEventListener('keydown', (e) => {
     if (sample) {
       playSample(sample);
     }
+    e.preventDefault();
   }
 }, false);
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(png|jpg|gif|svg)/,
+        use: ['url-loader'],
+      },
+      {
         test: /\.woff$/,
         use: ['url-loader'],
       },
